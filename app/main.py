9523807db9ace9499e51129e226bd22aeb0485a6
Rathi2323 <rathiamudhadt@gmail.com>
2025-12-01 19:51:14 +0530

    adding more endpoints

--- app/main.py
+++ app/main.py
@@ -6,6 +6,18 @@ app = Flask(__name__)
 def hello():
     return {"Message":"Hello Devops!"}
 
+@app.route("/health")
+def health():
+    return {"Status":"up"}
+
+@app.route("/info")
+def info():
+    return {"version":"1.2","commit":"abc123"}
+
+@app.route("/metrics")
+def metrics():
+    return "Promethus-ready metrics"
+
 if __name__ ==  "__main__":
     app.run(host="0.0.0.0", port = 5000)
 
